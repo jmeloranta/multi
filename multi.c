166706009d797f331c35f3eae53a815336c62c37
Jussi Eloranta <eloranta@aa6kj.hopto.org>
2026-04-26 17:58:18 -0500

    Added statistics collection. Rewrote the signal comparison routine.

--- multi.c
+++ multi.c
@@ -66,13 +66,8 @@
 
 #define JT9OPTS "-w 1 -m 2 -M -N 0 -D 1 -d 3 -X 1 -C 3"
 
-/*
- * Remove duplicate messages and retain the message with best report?
- * Use this with wsjt-x - not so useful in standalone mode.
- *
- */
-
-#define REMOVE_DUPES
+// #define COLLECT_STATS "/home/eloranta/stat.out" // Collect statistics of RX signal strengths?
+#define NOT_HEARD_DB (-26)   // If not heard, use this value for dB in stats
 
 #define STAT_LOC 21       // Character location in decode where to add the source
 
@@ -90,10 +85,15 @@
 #define MAX(a,b) (((a) > (b))?(a):(b))
 
 pid_t proc1, proc2;
-char *decodes[MAX_DECODES];
+#ifdef COLLECT_STATS
+int stat_fd = -1;
+#endif
 
 void cleanup(int x) {
 
+#ifdef COLLECT_STATS
+  close(stat_fd);
+#endif
   kill(proc1, SIGKILL);
   kill(proc2, SIGKILL);
   exit(0); // will also automatically free *decodes[]
@@ -115,9 +115,30 @@ void read_line(int fd, char *buf) {
   buf[i+1] = '\0';
 }
 
-int get_decodes(int fd, char id, int ndecodes) {
+#ifdef COLLECT_STATS
+void collect_stats(char *msg, int rpt1, int rpt2) {
 
-  int i = ndecodes, gotit = 0;
+  char *err = "Error opening stat file.\n";
+  char buf[128];
+
+  if(stat_fd == -1) {
+    unlink(COLLECT_STATS);
+    if((stat_fd = open(COLLECT_STATS, O_RDWR | O_CREAT, S_IRUSR | S_IWUSR)) < 0) {
+      write(2, err, strlen(err));
+      exit(0);
+    }
+  }
+  write(stat_fd, "# ", 2);
+  write(stat_fd, msg, strlen(msg));
+  write(stat_fd, "\n", 1);
+  sprintf(buf, "%d %d\n", rpt1, rpt2);
+  write(stat_fd, buf, strlen(buf));
+}
+#endif
+
+int get_decodes(int fd, char id, char *decodes[]) {
+
+  int i = 0, gotit = 0;
 
   while(!gotit) {
     read_line(fd, decodes[i]);
@@ -132,11 +153,11 @@ int get_decodes(int fd, char id, int ndecodes) {
   return i;
 }
 
-int show_decodes(int n) {
+int show_decodes(char *decodes[], int ndecodes) {
 
   int i, d = 0;
 
-  for(i = 0; i < n; i++)
+  for(i = 0; i < ndecodes; i++)
     if(*decodes[i] != '\0') {
       write(1, decodes[i], strlen(decodes[i]));
       d++;
@@ -156,51 +177,69 @@ int check_mycall(char *msg, char *mycall) {
   return 0;
 }
 
-void proc_decodes(int n) {
+void proc_decodes(char *decodes_1[], int ndecodes1, char *decodes_2[], int ndecodes2) {
 
   int i, j, rpt1, rpt2, diff;
   char msg1[128], msg2[128], *ptr;
 
-  for (i = 0; i < n; i++) {
-    if(*decodes[i] == '\0') {
-      i++;
+  // decodes_1 is RX a and decodes_2 RX b
+  for (j = 0; j < ndecodes2; j++) { // loop over RX 2 (slave)
+    if(check_mycall(decodes_2[j], CALL)) {
+      *decodes_2[j] = '\0'; // eliminate own call msgs on slave
       continue;
     }
-    if(check_mycall(decodes[i], CALL))
-      *decodes[i] = '\0'; // eliminate own call msgs on slave
-    else {
-      sscanf(decodes[i], "%*d %d %*f %*d %*s %[^\n]", &rpt1, msg1);
-      if((ptr = strstr(msg1, "     "))) *ptr = '\0'; // remove codes
-      for (j = i + 1; j < n; j++) {
-	if(*decodes[j] != '\0') {     
-	  sscanf(decodes[j], "%*d %d %*f %*d %*s %[^\n]", &rpt2, msg2);
-	  if((ptr = strstr(msg2, "     "))) *ptr = '\0';
-	  if(!strcmp(msg1, msg2)) {
-	    if(rpt1 > rpt2 && abs(rpt1-rpt2) >= EQUAL_THR) {
-	      *decodes[j] = '\0'; // i stronger
-	      if(abs(rpt1 - rpt2) >= UPCASE_THR) decodes[i][STAT_LOC] = toupper(decodes[i][STAT_LOC]);
-	    } else if(rpt1 < rpt2 && abs(rpt1-rpt2) >= EQUAL_THR) {
-	      *decodes[i] = '\0'; // j stronger
-	      if(abs(rpt1 - rpt2) >= UPCASE_THR) decodes[j][STAT_LOC] = toupper(decodes[j][STAT_LOC]);
-	    } else {
-	      *decodes[i] = '\0';
-              add_id(decodes[j], '=');  // indicate that a and b were equally strong
-	    }
-	    break;
-	  }
-	}
+    sscanf(decodes_2[j], "%*d %d %*f %*d %*s %[^\n]", &rpt2, msg2);
+    if((ptr = strstr(msg2, "     "))) *ptr = '\0'; // remove codes
+    for (i = 0; i < ndecodes1; i++) { // Loop over RX 1 (master)
+      if(*decodes_1[i] == '\0') continue;
+      sscanf(decodes_1[i], "%*d %d %*f %*d %*s %[^\n]", &rpt1, msg1);
+      if((ptr = strstr(msg1, "     "))) *ptr = '\0';
+      if(!strcmp(msg1, msg2)) {
+#ifdef COLLECT_STATS
+        collect_stats(msg1, rpt1, rpt2);
+#endif
+        if(rpt1 > rpt2 && abs(rpt1-rpt2) >= EQUAL_THR) {
+          *decodes_2[j] = '\0'; // i stronger
+          if(abs(rpt1 - rpt2) >= UPCASE_THR) decodes_1[i][STAT_LOC] = 'A';
+          else decodes_1[i][STAT_LOC] = 'a';
+        } else if(rpt1 < rpt2 && abs(rpt1-rpt2) >= EQUAL_THR) {
+          *decodes_1[i] = '\0'; // j stronger
+          if(abs(rpt1 - rpt2) >= UPCASE_THR) decodes_2[j][STAT_LOC] = 'B';
+          else decodes_2[j][STAT_LOC] = 'b';
+        } else {
+          *decodes_1[i] = '\0';
+          add_id(decodes_2[j], '=');  // indicate that a and b were equally strong
+        }
+        break;
       }
-      if(j == n) decodes[i][STAT_LOC] = toupper(decodes[i][STAT_LOC]); // the other RX did not receive
+    }
+    if(i == ndecodes1) {
+#ifdef COLLECT_STATS
+      collect_stats(msg2, NOT_HEARD_DB, rpt2); // head only on slave
+#endif
+      decodes_2[j][STAT_LOC] = toupper(decodes_2[j][STAT_LOC]); // the other RX did not receive
     }
   }
+
+  // Still missing received on RX 1 but not on 2.
+  for (i = 0; i < ndecodes1; i++)
+    if(*decodes_1[i] != '\0' && decodes_1[i][STAT_LOC] == '~') {
+#ifdef COLLECT_STATS
+      sscanf(decodes_1[i], "%*d %d %*f %*d %*s %[^\n]", &rpt1, msg1);
+      collect_stats(msg1, rpt1, NOT_HEARD_DB); // heard only on master
+#endif
+      decodes_1[i][STAT_LOC] = 'B';
+    }
 }
 
 int main(int argc, char **argv) {
 
-  int p1[2], p2[2], ndecodes, sync_time = -1, i;
+  int p1[2], p2[2], sync_time = -1, i;
   char buf[512];
   struct timeval tv;
   fd_set fds;
+  char *decodes_1[MAX_DECODES], *decodes_2[MAX_DECODES];
+  int ndecodes1, ndecodes2;
 
   if(!strncmp(argv[2], "WSJT-X - 2", 10)) {
     while(1) sleep(100); // do nothing - 2nd wsjt-x instance can be just minimized & ignored
@@ -236,35 +275,38 @@ int main(int argc, char **argv) {
   }
   signal(SIGINT, cleanup);
 
-  for (i = 0; i < MAX_DECODES; i++)
-    decodes[i] = (char *) malloc(sizeof(char) * 128);
-  
+  for (i = 0; i < MAX_DECODES; i++) {
+    decodes_1[i] = (char *) malloc(sizeof(char) * 128);
+    decodes_2[i] = (char *) malloc(sizeof(char) * 128);
+  }
+
   while(1) {
-    ndecodes = 0;
+    ndecodes1 = ndecodes2 = 0;
     FD_ZERO(&fds);
     FD_SET(p1[0], &fds);
     FD_SET(p2[0], &fds);
     select(MAX(p1[0],p2[0])+1, &fds, NULL, NULL, NULL);
     if(FD_ISSET(p1[0], &fds)) {
-      ndecodes = get_decodes(p1[0], 'a', ndecodes);
+      ndecodes1 = get_decodes(p1[0], 'a', decodes_1);
       FD_ZERO(&fds);
       FD_SET(p2[0], &fds);
       tv.tv_sec = PROCESS_SYNC;
       tv.tv_usec = 0;
       select(p2[0]+1, &fds, NULL, NULL, &tv);
-      if(FD_ISSET(p2[0], &fds)) ndecodes = get_decodes(p2[0], 'b', ndecodes);
+      if(FD_ISSET(p2[0], &fds)) ndecodes2 = get_decodes(p2[0], 'b', decodes_2);
     } else if(FD_ISSET(p2[0], &fds)) {
-      ndecodes = get_decodes(p2[0], 'b', ndecodes);
+      ndecodes2 = get_decodes(p2[0], 'b', decodes_2);
       FD_ZERO(&fds);
       FD_SET(p1[0], &fds);
       tv.tv_sec = PROCESS_SYNC;
       tv.tv_usec = 0;
       select(p1[0]+1, &fds, NULL, NULL, &tv);
-      if(FD_ISSET(p1[0], &fds)) ndecodes = get_decodes(p1[0], 'a', ndecodes);
+      if(FD_ISSET(p1[0], &fds)) ndecodes1 = get_decodes(p1[0], 'a', decodes_1);
     }
-    proc_decodes(ndecodes);
-    ndecodes = show_decodes(ndecodes);
-    sprintf(buf, "<DecodeFinished>   0  %d        0\n", ndecodes);
+    proc_decodes(decodes_1, ndecodes1, decodes_2, ndecodes2);
+    ndecodes1 = show_decodes(decodes_1, ndecodes1);
+    ndecodes2 = show_decodes(decodes_2, ndecodes2);
+    sprintf(buf, "<DecodeFinished>   0  %d        0\n", ndecodes1 + ndecodes2);
     write(1, buf, strlen(buf));
   }
 }
